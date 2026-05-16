5f935719e0ac866fcad8c4fbcfe351c8066d88b3
CodeBy-sreenath <sreenathts655@gmail.com>
2026-05-16 09:07:59 +0530

    minimum number of coins choose

---
+++ cost.py
@@ -0,0 +1,17 @@
+#how many coins will be selected for reach the minimum amount
+class Solution:
+    def minimumcost(self,costs,amount):
+        dp=[float('inf')]*(amount+1)
+        dp[0]=0
+        for i in range(1,amount+1):
+            for cost in costs:
+                if i>=cost:
+                    dp[i]=min(dp[i],dp[i-cost]+1)
+        if dp[amount]==float('inf'):
+            return -1
+        return dp[amount]
+s=Solution()
+costs=[1,2,5]
+amount=11
+print(s.minimumcost(costs,amount))                
+            
