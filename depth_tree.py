27405f6bdf8e0965b07fa4973f8e4b2d2945fd4a
CodeBy-sreenath <sreenathts655@gmail.com>
2026-05-19 09:08:57 +0530

    maximum depth of tree

---
+++ depth_tree.py
@@ -0,0 +1,29 @@
+from collections import deque
+class TreeNode:
+    def __init__(self,val,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+class Solution:        
+    def maxdept(self,root):
+        if not root:
+            return 0
+        que=deque()
+        que.append(root)
+        depth=0
+        while que:
+            for i in range(len(que)):
+                node=que.popleft()
+                if node.left:
+                    que.append(node.left)
+                if node.right:
+                    que.append(node.right)
+            depth+=1
+        return depth
+root=TreeNode(3)
+root.left=TreeNode(9)
+root.right=TreeNode(20)
+root.right.left=TreeNode(15)
+root.right.right=TreeNode(7)
+s=Solution()
+print(s.maxdept(root))                           
