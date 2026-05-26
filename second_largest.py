3a5713ec394c3d011ebc21a1cdb028fd3ead1819
CodeBy-sreenath <sreenathts655@gmail.com>
2026-05-26 19:40:34 +0530

    second_largest

---
+++ second_largest.py
@@ -0,0 +1,14 @@
+class Solution:
+    def second_largest(self,nums):
+        greatest_element=float('-inf')
+        second_greatest=float('-inf')
+        for num in nums:
+            if num>greatest_element:
+                second_greatest=greatest_element
+                greatest_element=num
+            if num>second_greatest and num!=greatest_element:
+                second_greatest=num
+        return second_greatest
+s=Solution()
+nums=[9,6,8,7,2]
+print(s.second_largest(nums))                
