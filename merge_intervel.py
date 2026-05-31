e36167839411884034e787329de24f791d5b9d69
CodeBy-sreenath <sreenathts655@gmail.com>
2026-05-31 10:58:05 +0530

    merge intervel changed1

--- merge_intervel.py
+++ merge_intervel.py
@@ -7,7 +7,7 @@ class Solution:
             if start<=last_end:
                 result[-1][1]=max(last_end,end)
             else:
-                result.append([start,end])
+                result.append([start,end]);
         return result
 s=Solution()
 print(s.merge([[1,3],[2,6],[8,10],[15,18]]))                
