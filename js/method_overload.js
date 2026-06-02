d12e465356c7b1b290d224703d2b666ba600db04
CodeBy-sreenath <sreenathts655@gmail.com>
2026-06-02 17:35:13 +0530

    method overloading in js

---
+++ js/method_overload.js
@@ -0,0 +1,12 @@
+class Maths
+{
+    add(a,b){
+        return a+b
+    }
+    add(a,b,c)
+    {
+        return a+b+c
+    }
+}
+const obj= new Maths()
+console.log(obj.add(1,2,3))
---
+++ js/sq.js
@@ -0,0 +1,10 @@
+n=4
+for(let i=0;i<n;i++)
+{
+    let row=""
+    for(j=0;j<n;j++)
+    {
+        row+="*"
+    }
+    console.log(row)
+}
