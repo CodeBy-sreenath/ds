06e6b27db30e9e504e2965f5f387a338a1e4fafd
CodeBy-sreenath <sreenathts655@gmail.com>
2026-06-02 11:06:23 +0530

    fibanocci

---
+++ js/fiba.js
@@ -0,0 +1,13 @@
+function fibanoci(n)
+{
+    let a=0
+    b=1
+    for(let i=0;i<=n;i++)
+    {
+        console.log(a)
+        let c=a+b
+        a=b
+        b=c
+    }
+}
+console.log(fibanoci(10))
